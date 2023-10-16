5e57b0e09e6f06a42d13c3676c5ec8fd14d0b2b5
heena63 <144784219+heena63@users.noreply.github.com>
2023-10-16 12:01:32 +0530

    Update main (4).py

--- challenge 3.1/main (4).py
+++ challenge 3.1/main (4).py
@@ -26,18 +26,3 @@ sorted_students = sort_students(students)
 for student in sorted_students:
   print(f"Name: {student.name}, Roll Number:{student.roll_number}, CGPA: {student.cgpa}")
 
-def linear_search_product(product_list, target_product):
-  indices = []
-  for i, product in enumerate(product_list):
-      if product == target_product:
-          indices.append(i)
-  return indices
-
-# Example usage:
-products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
-target_product = "Apple"
-
-result = linear_search_product(products, target_product)
-
-print(result)
-
